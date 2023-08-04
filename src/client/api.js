6deb485894ef86f2148287dea520c1aaf5a15261
ag076810 <88880369+ag076810@users.noreply.github.com>
2023-08-04 17:51:57 +0800

    Add files via upload

--- src/client/api.js
+++ src/client/api.js
@@ -2,10 +2,10 @@ class InfinityArcadeAPI {
     constructor() {
         if (document.location.href.indexOf("http://localhost:3000") == 0) {
             this.base_url = "http://localhost:3000/api";
-        } else if (document.location.href.indexOf("http://192.168.4.103:3000") == 0) {
-            this.base_url = "http://192.168.4.103:3000/api";1
+        } else if (document.location.href.indexOf("http://223.136.191.91:3000") == 0) {
+            this.base_url = "http://223.136.191.91:3000/api";1
         } else {
-            this.base_url = "https://infinityarcade.com/api";
+            this.base_url = "https://e286-2001-b400-e273-5f47-155-9130-f690-5aff.ngrok-free.app/api";
         }
     }
 
