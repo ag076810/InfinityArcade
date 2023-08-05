9001d147bff78ab7b6abd6ff876ffeb6ab0cab04
ag076810 <88880369+ag076810@users.noreply.github.com>
2023-08-05 21:51:45 +0800

    Add files via upload

--- src/sequelize.js
+++ src/sequelize.js
@@ -1,3 +1,4 @@
 const log = require("debug")("ia:sequelize");
 const { Sequelize } = require("sequelize");
+console.log(process.env.DATABASE_URI);
 module.exports = new Sequelize(process.env.DATABASE_URI);
