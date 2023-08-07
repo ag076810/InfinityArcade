e6879b6ded362110714ac05546eac33af115beb8
ag076810 <88880369+ag076810@users.noreply.github.com>
2023-08-07 17:27:07 +0800

    Update GenerateGame.js

--- src/services/GenerateGame.js
+++ src/services/GenerateGame.js
@@ -7,20 +7,23 @@ async function GenerateGame(prompt_text = null, model = process.env.AI_MODEL, pr
     console.log(`generating game (prompt_text=${prompt_text}, model=${model}, prompt_name=${prompt_name})...`);
 
     try {
+        console.log(prompt_name);
         const input = prompt.load(prompt_name, { prompt_text });
         const game = await AI(input, {
             model,
             parser: JSON.parse
         });
-
+        console.log("vfsfvfv");
         game.prompt_model = model;
         game.prompt_name = prompt_name;
         if (prompt_text) {
             game.prompt_text = prompt_text;
+            console.log("prompt_text");
         }
-
+        console.log("finish");
         return game;
     } catch (e) {
+        console.log(`error generating game ${e.message}`);
         log(`error generating game ${e.message}`);
         return null;
     }
