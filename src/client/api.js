22260e95e181761f0de67b165778ceeedf6248b0
ag076810 <88880369+ag076810@users.noreply.github.com>
2023-08-09 00:29:27 +0800

    Update api.js

--- src/client/api.js
+++ src/client/api.js
@@ -1,6 +1,6 @@
 class InfinityArcadeAPI {
     constructor() {
-        this.base_url = "https://3781cbc2.cpolar.io/api";
+        this.base_url = "https://aistory-5a6a.onrender.com/api";
     }	
 
     async fetch(url, data = null) {
